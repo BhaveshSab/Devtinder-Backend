f3ee23a8c39c4def1eee1ff690a5c9afd0e4b146
Bhavesh Sabnani <bhavesh.cool2005@gmail.com>
2026-06-30 20:50:41 +0530

    Your commit message here

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,13 @@
-require("dotenv").config();
+
 const mongoose = require("mongoose");
+const dns = require("dns");
+dns.setServers(["1.1.1.1" , "8.8.8.8"]);
 
 // code to connect to mongoDB atlas
 const connectDB = async () => {
   try {
-    const connect = await mongoose.connect("mongodb://localhost:27017/devTinder");
+    const connect = await mongoose.connect("mongodb+srv://bhaveshcool2005_db_user:f7PjqPo9FQOQ0D5y@cluster0.i30bks9.mongodb.net/"
+);
     console.log(
       "Database connected"
     );
